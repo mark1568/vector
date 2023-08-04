eb1ebbcbfb0a1b1e08e7a00cfd8e9d3879453b70
feifei <1938943744@qq.com>
2023-08-04 14:38:49 +0800

    1.0

---
+++ STL容器/STL存放自定义数据类型.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#include <algorithm>
+
+
+class Person
+{
+public:
+	Person(string name, int age)
+	{
+		this->m_name = name;
+		this->m_age = age;
+	}
+	string m_name;
+	int m_age;
+};
+
+void test01()
+{
+	vector<Person> v;
+	Person p1("aaa", 10);
+	Person p2("bbb", 20);
+	Person p3("ccc", 30);
+	Person p4("ddd", 40);
+	Person p5("fff", 50);
+
+	v.push_back(p1);
+	v.push_back(p2);
+	v.push_back(p3);
+	v.push_back(p4);
+	v.push_back(p5);
+
+	for (vector<Person>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		cout << "姓名：" << (*it).m_name <<"\t" << "年龄：" << it->m_age << endl;
+	}
+
+}
+
+void test02()
+{
+	vector<Person*> v;
+	Person p1("aaa", 10);
+	Person p2("bbb", 20);
+	Person p3("ccc", 30);
+	Person p4("ddd", 40);
+	Person p5("fff", 50);
+
+	v.push_back(&p1);
+	v.push_back(&p2);
+	v.push_back(&p3);
+	v.push_back(&p4);
+	v.push_back(&p5);
+
+	for (vector<Person*>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		cout << "姓名：" << (*it)->m_name << "\t" << "年龄：" << (*it)->m_age << endl;
+	}
+}
+
+
+//int main()
+//{
+//	test01();
+//	system("pause");
+//
+//	return 0;
+//}
